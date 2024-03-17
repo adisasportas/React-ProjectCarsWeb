28b6c8295c288e523eb81f4e36771486582f47d6
adisa <adi.sasportas8@icloud.com>
2024-03-17 19:22:47 +0200

    Aboutme

--- src/components/screens/AboutMe.js
+++ src/components/screens/AboutMe.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import '../screensCss/AboutMe.css'; // ייבוא קובץ ה-CSS
 
 export default function AboutMe() {
   return (
-    <div>
-      
+    <div className="about-me-container">
+      <div className="about-me-content">
+        <h2 className="about-me-heading">ABOUT ME</h2>
+        <p className="about-me-paragraph">
+          Welcome to my About Me page! I have always had a deep fascination with cars ever since I was a child. Growing up, I was surrounded by family members who were car enthusiasts, and that passion quickly rubbed off on me.
+        </p>
+        <p className="about-me-paragraph">
+          For me, cars represent more than just a mode of transportation. They symbolize freedom, adventure, and craftsmanship. I love the thrill of driving, the sound of a powerful engine, and the feeling of control behind the wheel.
+        </p>
+        <p className="about-me-paragraph">
+          My interest in cars extends beyond just driving. I enjoy learning about different makes and models, understanding their engineering, and staying up-to-date with the latest automotive technologies and trends.
+        </p>
+        <p className="about-me-paragraph">
+          Whether it's attending car shows, tinkering with engines, or simply going for a long drive on a scenic route, cars have always been a significant part of my life and continue to be a source of joy and inspiration for me.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/screens/AddCar.js
+++ src/components/screens/AddCar.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function AddCar() {
+const AddCar = ({ onAddCar }) => {
+  const [brend, setBrend] = useState('');
+  const [model, setModel] = useState('');
+  const [year, setYear] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newCar = { brend, model, year };
+    onAddCar(newCar);
+    setBrend('');
+    setModel('');
+    setYear('');
+  };
   return (
     <div>
-      
+      <h2>Add Car</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Brand:</label>
+          <input type="text" value={brend} onChange={(e) => setBrend(e.target.value)} />
+        </div>
+        <div>
+          <label>Model:</label>
+          <input type="text" value={model} onChange={(e) => setModel(e.target.value)} />
+        </div>
+        <div>
+          <label>Year:</label>
+          <input type="text" value={year} onChange={(e) => setYear(e.target.value)} />
+        </div>
+        <button type="submit">Add Car</button>
+      </form>
     </div>
-  )
-}
+  );
+};
+
+export default AddCar;
