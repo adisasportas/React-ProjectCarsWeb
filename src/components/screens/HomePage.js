3962b0e6d6413e2cfd27e1132520eefbd8e42a3f
adisa <adi.sasportas8@icloud.com>
2024-03-18 21:41:44 +0200

    FINISH

--- src/components/screens/HomePage.js
+++ src/components/screens/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div className="home-page">
       <div className="content">
-        <h1 className="title">Welcome to My Cars App!</h1>
+        <h1 className="title">Welcome to My Car App!</h1>
         <p className="description">Explore the world of cars with us.</p>
       </div>
     </div>
